06475edd0b34809471534b14c4bbc5ecaf4c715d
Diego Pagini <diegopagini777@gmail.com>
2021-01-01 07:27:51 -0300

    quedo funcionando, para tener en cuenta lo de auth0

--- src/app/components/protected/protected.component.ts
+++ src/app/components/protected/protected.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-protected',
   templateUrl: './protected.component.html',
-  styleUrls: ['./protected.component.scss']
+  styleUrls: ['./protected.component.scss'],
 })
 export class ProtectedComponent implements OnInit {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
